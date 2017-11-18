689f03e37df544545b7b3eccfef09c6c43d7baa2
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-18 14:30:58 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,9 +6,7 @@
 <?php 
     if (!isset($_COOKIE['count']))
     {
-
-      echo "Benvenuto! Questa e' la prima volta che accedi a questo sito!";
-
+        echo "Benvenuto! Questa e' la prima volta che accedi a questo sito!";
         $cookie = 1;
         setcookie("count", $cookie,time()+60*60*24*7);
     }
@@ -16,8 +14,7 @@
     {
         $cookie = $_COOKIE['count']++;
         setcookie("count", $cookie);
-    
-      echo "You have viewed this page <?= $_COOKIE['count'] ?> times."; 
+        echo "Hai visitato questa pagina $cookie volte."; 
     }  
       ?> 
    </body> 
