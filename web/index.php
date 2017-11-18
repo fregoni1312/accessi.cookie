bf52ccadd2e5b216ac5421be7f3b079029dda4f0
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-18 14:10:16 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html PUBLIC "=//W3C//DTD XHMTL 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhmtl" xml:lang="en">
+<html >
 <head><title>PHP Script using Cookies</title>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 </head>
 <body>
 <?php
-echo “<p> A cookie has been set. </p>”;
+echo $value;
+echo "                          ";
+setcookie("n_accessi", $value, time()+60*60*24*7);
+echo $value;
 ?>
 </body>
 </html>
