fe18e25732235560bacb8d7cc770dc07d6ebe9f6
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-18 14:15:07 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 $value=0;
 echo $value;
 echo "                          ";
-//setcookie("n_accessi", $value, time()+60*60*24*7);
+setcookie("n_accessi", $value, time()+60*60*24*7);
 echo $value;
 ?>
 
