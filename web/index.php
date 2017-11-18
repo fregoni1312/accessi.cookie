f97f48254b9b4489a1d6ce3391ae82ba2701b6dc
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-18 14:17:14 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,27 @@
-<?php
-$value=0;
-echo $value;
-echo "                          ";
-setcookie("n_accessi", $value, time()+60*60*24*7);
-echo $value;
-?>
+<html> 
+    <head> 
+        <title>Count Page Access</title> 
+   </head> 
+  <body> 
+<?php 
 
+    if (!isset($_COOKIE['count']))
+    {
+        ?> 
+Welcome! This is the first time you have viewed this page. 
+<?php 
+        $cookie = 1;
+        setcookie("count", $cookie);
+    }
+    else
+    {
+        $cookie = $_COOKIE['count']++;
+        setcookie("count", $cookie);
+        ?> 
+You have viewed this page <?= $_COOKIE['count'] ?> times. 
+<?php  }// end else  ?> 
+   </body> 
+</html>
 
 
 
