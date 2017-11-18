2a3c2771a5a793bde02c15d0f8711aada3a6c755
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-18 14:13:12 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,16 +1,9 @@
-<html >
-<head><title>PHP Script using Cookies</title>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-</head>
-<body>
 <?php
 echo $value;
 echo "                          ";
-setcookie("n_accessi", $value, time()+60*60*24*7);
+//setcookie("n_accessi", $value, time()+60*60*24*7);
 echo $value;
 ?>
-</body>
-</html>
 
 
 
